f1698845f7628f46833ca8b2c85ecc697b968e86
Ali Alsayed <a_alsayed87@hotmail.com>
2019-03-16 08:36:06 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -7,7 +7,7 @@ class CouponsController < ApplicationController
   end
 
   def create
-    @coupon = Coupon.new 
+    @coupon = Coupon.new
     @coupon.coupon_code = params[:coupon][:coupon_code]
     @coupon.store = params[:coupon][:store]
     redirect_to @coupon
